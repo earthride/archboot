66145c36c16035f63fd61dd57608d7677d84bae0
earthride <39838515+earthride@users.noreply.github.com>
2020-02-17 21:30:42 +0100

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-function pause(){
-   read -p "$*"
-}
+set -euo pipefail
+IFS=$'\n\t'
+
+sgdisk --zap-all "$1"
+wipefs -a "$1"
 
 timedatectl set-ntp true
 
@@ -11,29 +13,28 @@ sgdisk -n 1:0:+550MiB -c 1:"EFIBOOT" -t 1:ef00 "$1"
 sgdisk -n 2:0:+2GiB -c 2:"root" -t 2:8304 "$1"
 sgdisk -n 3:0:+3GiB -c 3:"/tmp" -t 3:8300 "$1"
 sgdisk -n 4:0:+4GiB -c 4:"/home" -t 4:8302 "$1"
-sgdisk -p "$1"
 
-mkfs.fat -F32 "/dev/sda1"
-mkfs.ext4 "/dev/sda2"
-mkfs.ext4 "/dev/sda3"
-mkfs.ext4 "/dev/sda4"
+mkfs.fat -F32 /dev/sda1
+mkfs.ext4 /dev/sda2
+mkfs.ext4 /dev/sda3
+mkfs.ext4 /dev/sda4
 
-mount "/dev/sda2" /mnt
+mount /dev/sda2 /mnt
 mkdir -p /mnt/boot
-mount "/dev/sda1" /mnt/boot
+mount /dev/sda1 /mnt/boot
 mkdir -p /mnt/tmp
-mount "/dev/sda3" /mnt/tmp
+mount /dev/sda3 /mnt/tmp
 mkdir -p /mnt/home
-mount "/dev/sda4" /mnt/home
+mount /dev/sda4 /mnt/home
 
-pause 'Done Partitioning. Press [Enter] to continue...'
+pacman --noconfirm -Sy archlinux-keyring >/dev/null 2>&1
 
-pacstrap /mnt #base linux linux-firmware intel-ucode 
+pacstrap /mnt base linux linux-firmware intel-ucode sudo networkmanager reflector
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
-pause 'Chrooting into the system. Press [Enter] to continue...'
-
 curl https://raw.githubusercontent.com/earthride/archboot/master/chroot.sh > /mnt/chroot.sh
 arch-chroot /mnt bash ./chroot.sh && rm /mnt/chroot.sh
-arch-chroot /mnt 
+
+umount -R /mnt
+
