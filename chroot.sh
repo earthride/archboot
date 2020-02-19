3ec658c24f2dd56baef1650f37b9c58c2c9e6581
earthride <39838515+earthride@users.noreply.github.com>
2020-02-19 11:55:40 +0100

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -41,9 +41,4 @@ pacman --noconfirm -Sy archlinux-keyring >/dev/null 2>&1
 reflector --latest 20 --sort score --age 24 --save /etc/pacman.d/mirrorlist
 pacman -S --noconfirm --needed xorg-server xorg-xinit git neovim nm-connection-editor firewalld zsh network-manager-applet bspwm feh gedit
 
-systemctl enable fstrim.timer firewalld.service NetworkManager
-
-mkdir -p home/earthride/.config/{bspwm,sxhkd}
-cp /usr/local/share/doc/bspwm/examples/bspwmrc home/earthride/.config/bspwm/
-cp /usr/local/share/doc/bspwm/examples/sxhkdrc home/earthride/.config/sxhkd/
-chmod u+x home/earthride/.config/bspwm/bspwmrc
+systemctl enable fstrim.timer firewalld.service NetworkManage
